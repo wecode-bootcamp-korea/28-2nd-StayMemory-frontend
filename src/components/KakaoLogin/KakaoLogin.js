26c77c7cd51e1121106bb1dd5b5640ebf03a06db
채현 <ogchaehyeon@ogchaehyeon-ui-MacBookPro.local>
2022-01-10 21:31:07 +0900

    [28기 옥채현] Add: 마이페이지 위시리스트 api 연결, 페이지네이션 구현 및 통신, welcome 문구 통신완료
    [28기 옥채현] 임시
    
    [28기 옥채현] Mypage 레이아웃 구현
    
    [28기 옥채현] 백엔드와 소통 후 key값과 데이터 수정
    
    [28기 옥채현] 임시
    
    [28기 옥채현] Add: 위시리스트 api연결
    
    [28기 옥채현] Add: 페이지네이션 구현
    
    [28기 옥채현] Add: welcome 문구 백엔드 통신
    
    [28기 옥채현] Add: welcome 문구 백엔드 통신완료
    
    [28기 옥채현] Modify:리뷰 반영
    
    [28기 옥채현] Add:로그인 페이지 레이아웃
    
    [28기 옥채현] Add: keyframes 이용 title 애니메이션
    
    [28기 옥채현] Add: keyframes 이용 title 애니메이션
    
    [Add] 액세스 토큰 발급 완료
    
    [28기 옥채현] Add: 소셜 로그인
    
    [28기 옥채현] Add: 카카오 소셜 로그인 버튼 css
    
    [28기 옥채현] 임시 주석

--- src/components/KakaoLogin/KakaoLogin.js
+++ src/components/KakaoLogin/KakaoLogin.js
@@ -1,44 +1,23 @@
-import { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-
+import { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 function KakaoLogin() {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [accessToken, setAccessToken] = useState('');
-
+  const [searchParams, setSearchParams] = useSearchParams();
+  const API_KEY = process.env.REACT_APP_KAKAO_API_KEY;
+  const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+  // const [searchParams, setSearchParams] = useSearchParams();
   useEffect(() => {
-    const authCode = location.search.slice(6);
-    function getToken() {
-      fetch('https://kauth.kakao.com/oauth/token', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-        },
-        body: `grant_type=authorization_code&client_id=${process.env.REACT_APP_KAKAO_API_KEY}&code=${authCode}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`,
-      })
-        .then(res => res.json())
-        .then(res => setAccessToken(res.access_token));
-    }
-    getToken();
-  }, [location.search]);
+    //console.log(searchParams);
 
-  useEffect(() => {
-    function sendToken() {
-      fetch('http://192.168.243.37:8080/users/signin-kakao', {
-        method: 'POST',
-        headers: {
-          Authorization: accessToken,
-        },
-      }).then(res => res.json());
-    }
-    sendToken();
-    goToMain();
-  }, [accessToken]);
-
-  function goToMain() {
-    navigate('/');
-  }
-
+    fetch('https://kauth.kakao.com/oauth/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+      },
+      body: `grant_type=authorization_code&client_id=${API_KEY}&${searchParams}&client_secret=${CLIENT_SECRET}`,
+    }).then(res => res.json());
+    //.then(res => console.log(res));
+  }, [searchParams]);
+  // console.log(searchParams.get('code'));
   return null;
 }
 
