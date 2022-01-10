2306d484ee76f4e7046596df74b500421df68f74
채현 <ogchaehyeon@ogchaehyeon-ui-MacBookPro.local>
2022-01-10 21:31:07 +0900

    소셜 로그인 인가코드/토큰 발급
    Add:로그인 페이지 레이아웃
    
    Add: keyframes 이용 title 애니메이션
    
    액세스 토큰 발급 완료
    
    Add: 소셜 로그인
    
    [Modify] 앱 키 수정
    
    [Fix] 인가코드 발급 오류 수정
    
    [Fix] 토큰 발급 오류 수정
    
    [Add] 백엔드 토큰 전송 테스트
    
    [Refactor] 코드 포맷 수정
     - 불필요한 변수 삭제
     - Oath.js삭제
    
    [Fix] 로그인 오류 추가
     - 누락된 env파일 추가
     - 인가 코드 발급 오류 수정: env파일 변경사항 반영되지 않았음
    
    [Refactor] 코드 포맷 수정
    
    [Add] 백엔드 통신 성공, 로그인 후 페이지 이동 기능 추가

--- src/components/KakaoLogin/KakaoLogin.js
+++ src/components/KakaoLogin/KakaoLogin.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function KakaoLogin() {
-  return <div>kakaologin</div>;
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [accessToken, setAccessToken] = useState('');
+
+  useEffect(() => {
+    const authCode = location.search.slice(6);
+    function getToken() {
+      fetch('https://kauth.kakao.com/oauth/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+        },
+        body: `grant_type=authorization_code&client_id=${process.env.REACT_APP_KAKAO_API_KEY}&code=${authCode}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`,
+      })
+        .then(res => res.json())
+        .then(res => setAccessToken(res.access_token));
+    }
+    getToken();
+  }, [location.search]);
+
+  useEffect(() => {
+    function sendToken() {
+      fetch('http://192.168.243.37:8080/users/signin-kakao', {
+        method: 'POST',
+        headers: {
+          Authorization: accessToken,
+        },
+      }).then(res => res.json());
+    }
+    sendToken();
+    goToMain();
+  }, [accessToken]);
+
+  function goToMain() {
+    navigate('/');
+  }
+
+  return null;
 }
 
 export default KakaoLogin;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,86 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
 
 function Login() {
-  return <div>login</div>;
+  const KAKAO_LOGIN_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=code`;
+
+  return (
+    <Container>
+      <div>
+        <ContentAnimationBox>
+          <TitleContainer>
+            <EngTitle>LOGIN</EngTitle>
+            <KorTitle>로그인</KorTitle>
+          </TitleContainer>
+        </ContentAnimationBox>
+
+        <LoginForm>
+          <LoginInput placeholder="이메일 아이디" />
+          <LoginInput placeholder="비밀번호" />
+          <LoginButton>LOGIN</LoginButton>
+          <a href={KAKAO_LOGIN_URL}>카카오로그인</a>
+        </LoginForm>
+      </div>
+    </Container>
+  );
 }
 
 export default Login;
+
+const sayHelloAnimation = keyframes`
+  from {
+    transform: rotate(-5deg);
+  }to{
+    transform: rotate(5deg);
+  }
+`;
+
+const ContentAnimationBox = styled.div`
+  animation: ${sayHelloAnimation} 1s linear infinite alternate;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const TitleContainer = styled.div`
+  margin-top: 30px;
+`;
+
+const Title = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const EngTitle = styled(Title)`
+  font-size: 25px;
+`;
+
+const KorTitle = styled(Title)`
+  font-size: 15px;
+  color: gray;
+`;
+
+const LoginForm = styled.div`
+  border-top: 1px solid black;
+  margin-top: 40px;
+  padding-top: 30px;
+  display: flex;
+  flex-direction: column;
+  width: 360px;
+`;
+
+const LoginInput = styled.input`
+  height: 45px;
+  border: none;
+  border-bottom: 1px solid gray;
+`;
+
+const LoginButton = styled.button`
+  margin-top: 30px;
+  width: 360px;
+  height: 55px;
+  background-color: black;
+  color: white;
+`;
