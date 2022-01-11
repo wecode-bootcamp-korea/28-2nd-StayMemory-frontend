32becbc445aa84fd9cc1bac83a8cf6000e7d664b
gy <gayunlee11@gmail.com>
2022-01-11 13:43:43 +0900

    [Add] nav 컴포넌트, 달력 구현
    [Modify] css 수정
     - border-left 추가
     - padding 값 추가
    
    [Add] 달력 라이브러리 설치
    
    [Add] 시작/종료 날짜 문자열 변환 완료
    
    [Modify] 선택 기간 색상 변경
     - 선택한 기간을 처리할 수 있도록  조건부 함수로 변경
    
    [Add] 필터 창 구현
     - 팝업 컴포넌트 분리
     - 달력 컴포넌트 변경
     - 어디로 떠날까요? 언제 떠날까요? 팝업창 레이아웃 구현 완료
    
    [Add] Nav 컴포넌트로 위치 변경
     - position fixed로 변경
    
    [Refactor] 코드 포맷 정리
    
    [Modify]
     - mode button 추가
     - 글씨 크기 조정
     - 로그인 상태에 따른 로그인/로그아웃 및 유저 아이콘 조건부 렌더링 추가
    
    [Refactor] 캘린더 컴포넌트 변경
     - 기존 라이브러리 전체 삭제
     - 컴포넌트 명 변경
    
    [Refactor] 코드 포맷 수정
     - 불필요한 변수 삭제
     - 불필요한 import문 삭제
    
    [Add] 다크모드 버튼 토글 기능 추가

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { HiUser, HiOutlineLocationMarker } from 'react-icons/hi';
+import { AiOutlineCalendar } from 'react-icons/ai';
+import { BsToggleOn, BsToggleOff } from 'react-icons/bs';
 
 function Nav() {
-  return <div className="logo">logo</div>;
+  const isLoggedIn = sessionStorage.getItem('access_token');
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  function handleDarkMode() {
+    setIsDarkMode(!isDarkMode);
+  }
+  return (
+    <Wrapper>
+      <div className="logo">logo</div>
+      <FilterWrap>
+        <span className="location">
+          <HiOutlineLocationMarker />
+          <p>어디로 떠날까요?</p>
+        </span>
+        <span className="date">
+          <AiOutlineCalendar />
+          <p>언제 떠날까요?</p>
+        </span>
+      </FilterWrap>
+      <MenuWrap>
+        <span>FIND STAY</span>
+        <span>PROMOTION</span>
+        <span>JOURNAL</span>
+        <span>PRE-ORDER</span>
+      </MenuWrap>
+      <UserWrap>
+        {isLoggedIn && <HiUser />}
+        <button className="login">{isLoggedIn ? 'LOGOUT' : 'LOGIN'}</button>
+        {isDarkMode ? (
+          <BsToggleOff onClick={handleDarkMode} />
+        ) : (
+          <BsToggleOn onClick={handleDarkMode} />
+        )}
+      </UserWrap>
+      {/* <PopUp component={<Calendar2 />} title="언제 떠날까요?" /> */}
+      {/* <PopUp component={<Location />} title="어디로 떠날까요?" /> */}
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-width: 80rem;
+  height: 5rem;
+  padding: 0 3rem 0 3rem;
+  border-bottom: 1px solid lightgrey;
+  font-size: 0.9rem;
+`;
+
+const FilterWrap = styled.div`
+  display: flex;
+
+  & > span {
+    display: flex;
+    align-items: center;
+    padding: 0 1.3rem 0 1.3rem;
+  }
+
+  & > span:nth-child(2) {
+    border-left: 1px solid lightgrey;
+  }
+
+  & > span > svg {
+    margin-right: 0.3rem;
+    font-size: 1.5rem;
+  }
+`;
+
+const MenuWrap = styled.span`
+  display: flex;
+  justify-content: space-around;
+
+  & > span {
+    padding: 0 0.5rem 0 0.5rem;
+    font-weight: 600;
+  }
+`;
+
+const UserWrap = styled.span`
+  display: flex;
+  align-items: center;
+  padding-left: 2.6rem;
+  border-left: 1px solid lightgrey;
+
+  & > button {
+    background-color: transparent;
+    border: none;
+    font-weight: 600;
+    /* font-size: 1.2rem; */
+  }
+
+  & > svg {
+    margin-left: 0.4rem;
+    font-size: 1.5rem;
+  }
+`;
 
 export default Nav;
