040d4515635d786bc6f7a8fbbbf839ba719596c8
gy <gayunlee11@gmail.com>
2022-01-13 14:39:56 +0900

    [Add] 날짜 선택 달력 추가
    [Fix] 로그인 버그 수정
    
    [Add] 선택 불가능한 날짜 경고 메시지 처리
    
    [Modify] css 수정
     - min/max width 값 추가
     - 아래 보더 보이지 않는 오류 수정
     - 돌아가기 아이콘 추가
     - 가운데정렬
    
    [Fix] 라이브러리 설치 변경
    
    [Refactor] 컴포넌트 변경 : SelectDate
    
    [Fix] 달력 라이브러리 설치 오류 수정
    
    [Add] FindStay 페이지 캘린더 컴포넌트 추가
    
    [Refactor] 코드 포맷 수정
     - 사용하지 않는 변수 삭제
     - 불필요한 import 문 삭제
    
    [Add] 목업데이터 추가
    
    [Fix] git conflict 해결
    
    [Fix] 충돌수정 - package.json
    
    [Fix] selectDate 누락된 코드 복구

--- src/components/SelectDate/SelectDate.js
+++ src/components/SelectDate/SelectDate.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from 'react-date-range-calendar';
+import styled from 'styled-components';
 
-function Calendar() {
-  return <div>calendar</div>;
+function SelectDate() {
+  const disabledDatesArr = ['2022-01-21', '2022-01-20'];
+  const [isAllAbleDate, setIsAllAbleDate] = useState(true);
+  function validDate(dates) {
+    disabledDatesArr.some(disabledDate => !dates.includes(disabledDate))
+      ? setIsAllAbleDate(true)
+      : setIsAllAbleDate(false);
+  }
+
+  return (
+    <Wrapper>
+      <p>{isAllAbleDate || '예약 불가능한 날짜가 포함되었습니다.'}</p>
+      <Calendar
+        onSelect={(startDate, endDate, validDateRange) => {
+          validDate(validDateRange);
+        }}
+        selectedRange={[null, null]}
+        disablePrevDates={true}
+        disabledDates={() => {
+          return ['2022-01-21'];
+        }}
+        startDateTdCssObj={{
+          backgroundColor: 'black',
+          borderRadius: '1rem 0 0 0',
+        }}
+        endDateTdCssObj={{
+          backgroundColor: 'black',
+          borderRadius: '0 0 1rem 0',
+        }}
+        inRangedTdCssObj={{ backgroundColor: 'black' }}
+      />
+      <button>SEARCH</button>
+    </Wrapper>
+  );
 }
 
-export default Calendar;
+const Wrapper = styled.div`
+  //
+`;
+
+export default SelectDate;
